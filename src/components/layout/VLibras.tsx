d06ff831a5606a40c873a8bc7cb0ff3200973faa
Giovanni Cavelagna <gcavelagna@gmail.com>
2025-08-11 19:53:28 +0000

    // Exemplo simples do componente VLibras para React/Next.js
    import Scri

--- src/components/layout/VLibras.tsx
+++ src/components/layout/VLibras.tsx
@@ -2,57 +2,27 @@
 "use client";
 
 import Script from 'next/script';
-import { useEffect } from 'react';
 
 export function VLibras() {
-
-  useEffect(() => {
-    // This effect ensures the script is re-initialized on component mount if needed,
-    // especially during client-side navigation.
-    const initializeVLibras = () => {
-      // @ts-ignore
-      if (window.VLibras && typeof window.VLibras.Widget === 'function') {
-        // @ts-ignore
-        new window.VLibras.Widget('https://vlibras.gov.br/app');
-      }
-    };
-    
-    // Check if the script is already loaded
-    const script = document.querySelector('script[src="https://vlibras.gov.br/app/vlibras-plugin.js"]');
-    if (script) {
-      // If script is loaded but widget not initialized, try initializing
-      // @ts-ignore
-      if (!window.VLibras) {
-         script.addEventListener('load', initializeVLibras);
-      } else {
-         initializeVLibras();
-      }
-    }
-
-    return () => {
-       if (script) {
-         script.removeEventListener('load', initializeVLibras);
-       }
-    }
-  }, []);
-
   return (
     <>
-      <div vw-access-button="true" className="active"></div>
-      <div vw-plugin-wrapper="true">
-        <div className="vw-plugin-top-wrapper"></div>
+      {/* Widget VLibras - Bonequinho de Libras */}
+      <div vw="true" className="enabled">
+        <div vw-access-button="true" className="active"></div>
+        <div vw-plugin-wrapper="true">
+          <div className="vw-plugin-top-wrapper"></div>
+        </div>
       </div>
-      <Script 
-        src="https://vlibras.gov.br/app/vlibras-plugin.js" 
+
+      {/* Script oficial do VLibras */}
+      <Script
+        src="https://vlibras.gov.br/app/vlibras-plugin.js"
         strategy="afterInteractive"
         onLoad={() => {
           // @ts-ignore
-          if (window.VLibras) {
-            // @ts-ignore
-            new window.VLibras.Widget('https://vlibras.gov.br/app');
-          }
+          new window.VLibras.Widget('https://vlibras.gov.br/app');
         }}
       />
     </>
-  )
+  );
 }
