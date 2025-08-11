bc3222cd810a94928236db328d79a04b6a113695
Giovanni Cavelagna <gcavelagna@gmail.com>
2025-08-11 20:02:36 +0000

    o ator do VLIBRAS não esta aparecendo

--- src/components/layout/VLibras.tsx
+++ src/components/layout/VLibras.tsx
@@ -1,26 +1,38 @@
 
 "use client";
 
+import React, { useEffect } from 'react';
 import Script from 'next/script';
 
 export function VLibras() {
+  const vlibrasContainerId = 'vlibras-widget-container';
+
+  useEffect(() => {
+    const container = document.getElementById(vlibrasContainerId);
+    if (container) {
+      container.innerHTML = `
+        <div vw="true" className="enabled">
+          <div vw-access-button="true" className="active"></div>
+          <div vw-plugin-wrapper="true">
+            <div className="vw-plugin-top-wrapper"></div>
+          </div>
+        </div>
+      `;
+    }
+  }, []);
+
   return (
     <>
-      {/* Widget VLibras - Bonequinho de Libras */}
-      <div vw="true" className="enabled">
-        <div vw-access-button="true" className="active"></div>
-        <div vw-plugin-wrapper="true">
-          <div className="vw-plugin-top-wrapper"></div>
-        </div>
-      </div>
-
-      {/* Script oficial do VLibras */}
+      <div id={vlibrasContainerId} />
       <Script
         src="https://vlibras.gov.br/app/vlibras-plugin.js"
         strategy="afterInteractive"
         onLoad={() => {
           // @ts-ignore
-          new window.VLibras.Widget('https://vlibras.gov.br/app');
+          if (window.VLibras) {
+            // @ts-ignore
+            new window.VLibras.Widget('https://vlibras.gov.br/app');
+          }
         }}
       />
     </>
